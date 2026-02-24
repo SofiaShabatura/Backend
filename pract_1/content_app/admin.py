29ea026e40aceedb5cbe84ef04b0d42c357c2ffa
SofiaShabatura <Sofiykashabatura@gmail.com>
2026-02-24 16:38:10 +0200

    episode,admin.py

--- pract_1/content_app/admin.py
+++ pract_1/content_app/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Content, Episode
 
-# Register your models here.
+admin.site.register(Content)
+admin.site.register(Episode)
--- pract_1/content_app/models.py
+++ pract_1/content_app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Content(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
@@ -8,6 +9,21 @@ class Content(models.Model):
 
     class Meta:
         db_table = "content"
+
     def __str__(self):
         return self.title
-    
+
+
+class Episode(models.Model):
+    content = models.ForeignKey(Content, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    release_date = models.DateField()
+    season_number = models.IntegerField()
+    episode_number = models.IntegerField()
+
+    class Meta:
+        db_table = "episode"
+
+    def __str__(self):
+        return f"{self.content.title} – Episode {self.episode_number}: {self.title}"
