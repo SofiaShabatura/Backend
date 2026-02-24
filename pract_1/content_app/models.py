35af3d4adfed2d3d8980d0435f16e8fe47d9fb98
SofiaShabatura <Sofiykashabatura@gmail.com>
2026-02-24 14:50:58 +0200

    added content_app and users_app models and migrations

--- pract_1/content_app/models.py
+++ pract_1/content_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Content(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    release_date = models.DateField()
+    genre = models.CharField(max_length=100)
+
+    class Meta:
+        db_table = "content"
+    def __str__(self):
+        return self.title
+    
--- pract_1/users_app/models.py
+++ pract_1/users_app/models.py
@@ -1,4 +1,11 @@
 from django.db import models
 
-# Create your models here.
-привіт постижися
+class User(models.Model):
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    class Meta:
+        db_table = "users"
+
+    def __str__(self):
+        return self.email
