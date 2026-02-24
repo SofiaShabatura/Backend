8c8c077c4e6c8c5f480c546c0dd47637ab579021
SofiaShabatura <Sofiykashabatura@gmail.com>
2026-02-24 17:12:18 +0200

    genres remake

--- pract_1/content_app/admin.py
+++ pract_1/content_app/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Content, Episode
+from .models import Content, Episode, Genre
 
 admin.site.register(Content)
 admin.site.register(Episode)
+admin.site.register(Genre)
+
--- pract_1/content_app/models.py
+++ pract_1/content_app/models.py
@@ -1,18 +1,35 @@
 from django.db import models
 
 
+class Genre(models.Model):
+    name = models.CharField(max_length=100)
+
+    class Meta:
+        db_table = "genre"
+
+    def __str__(self):
+            return self.name
+        
 class Content(models.Model):
+    MOVIE = 'movie'
+    TV_SHOW = 'tv_show'
+    SERIES = 'series'
+    TYPE_CHOICES = [
+        (MOVIE, 'Movie'),
+        (TV_SHOW, 'TV Show'),
+        (SERIES, 'Series'),
+    ]
+    type = models.CharField(max_length=20, choices=TYPE_CHOICES)
     title = models.CharField(max_length=255)
-    description = models.TextField()
+    description = models.TextField(blank=True)
     release_date = models.DateField()
-    genre = models.CharField(max_length=100)
+    genres = models.ManyToManyField("Genre", blank=True, related_name="contents")
 
     class Meta:
         db_table = "content"
 
     def __str__(self):
-        return self.title
-
+        return f"{self.title} ({self.type})"
 
 class Episode(models.Model):
     content = models.ForeignKey(Content, on_delete=models.CASCADE)
@@ -27,3 +44,4 @@ class Episode(models.Model):
 
     def __str__(self):
         return f"{self.content.title} – Episode {self.episode_number}: {self.title}"
+
