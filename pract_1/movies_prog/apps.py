16c9083bd2659aef0b6802992a862ed1d4d3a1f7
SofiaShabatura <Sofiykashabatura@gmail.com>
2026-02-24 14:25:30 +0200

    added users_app and content_app

--- pract_1/movies_prog/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class MoviesProgConfig(AppConfig):
-    name = 'movies_prog'
--- pract_1/pract_1/settings.py
+++ pract_1/pract_1/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'movies_prog',
+    'users_app',
+    'content_app',
+    
 ]
 
 MIDDLEWARE = [
